58e00b538e5766c248cc5365462dd3fb483ece01
conversayShawn <shawn.harris3510@gmail.com>
2024-07-24 08:16:21 -0400

    add a11y results

---
+++ scripts/verifyAccessibilityResults.js
@@ -0,0 +1,31 @@
+const { getAccessibilityResults } = require("@cypress/extract-cloud-results");
+
+getAccessibilityResults().then((results) => {
+  const { runNumber, accessibilityReportUrl, summary, rules } = results;
+  const { total } = summary.violationCounts;
+
+  console
+    .log(
+      `Received ${
+        summary.isPartialReport ? "partial" : ""
+      } results for run #${runNumber}.`
+    )
+    .console.log(`See full report at ${accessibilityReportUrl}.`);
+
+  // write your logic to conditionally fail based on the results
+  if (total > 0) {
+    const { critical, serious, moderate, minor } = summary.violationCounts;
+
+    console.log(
+      `${total} Accessibility violations were detected - ${critical} critical, ${serious} serious, ${moderate} moderate, and ${minor} minor.`
+    );
+    console.log("The following rules were violated:");
+    console.log(rules);
+
+    throw new Error(
+      `${total} Accessibility violations detected and must be fixed.`
+    );
+  }
+
+  console.log("No Accessibility violations detected!");
+});
