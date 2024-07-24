c7f30f98a2aea057875ffa4062302cd58b278fa5
conversayShawn <shawn.harris3510@gmail.com>
2024-07-24 11:10:39 -0400

    update tag

--- scripts/verifyAccessibilityResults.js
+++ scripts/verifyAccessibilityResults.js
@@ -3,7 +3,6 @@ const { getAccessibilityResults } = require('@cypress/extract-cloud-results');
 getAccessibilityResults({
   projectId: process.env.CYPRESS_PROJECT_ID,
   recordKey: process.env.CYPRESS_RECORD_KEY,
-  // runTags: [String(process.env.RUN_TAGS)],
   runTags: "a11y",
 }).then((results) => {
   const { runNumber, accessibilityReportUrl, summary, rules } = results;
